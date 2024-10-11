946e3454b2998d168b17a3dff76d5535f2a36162
Shaina <shaina@geekster.lan>
2024-10-11 11:28:23 +0200

    added DB Password environ

---
+++ cart/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
+
+from django.contrib import messages
+
+
+def cart_summary(request):
+    # Get the cart
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    totals = cart.cart_total()
+    return render(request, 'cart_summary.html', {"cart_products": cart_products, "quantities": quantities, "totals": totals})
+
+def cart_add(request):
+    # Get the cart
+    cart = Cart(request)
+    # test for POST
+    if request.POST.get('action') == 'post':
+        # Get stuff
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+
+        # Lookup product in DB
+        product = get_object_or_404(Product, id=product_id)
+
+        # Save to session
+        cart.add(product=product, quantity=product_qty)
+
+        # Get the quantity of items in the cart
+        cart_quantity = cart.__len__()
+
+        # response = JsonResponse({'Product Name:': product.name})
+        response = JsonResponse({'qty': cart_quantity})
+        messages.success(request, 'Item added to cart')
+
+    # Return response
+    return response
+
+def cart_delete(request):
+    # Implement the cart delete functionality here
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        # Call delete Function in Cart
+        cart.delete(product=product_id)
+
+        response = JsonResponse({'product': product_id})
+        # return redirect('cart_summary')
+        messages.success(request, 'Item deleted')
+        return response
+
+def cart_update(request):
+    # Implement the cart update functionality here
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('product_id'))
+        product_qty = int(request.POST.get('product_qty'))
+
+        # Call update Function in Cart
+        cart.update(product=product_id, quantity=product_qty)
+
+        response = JsonResponse({'qty': product_qty})
+        # return redirect('cart_summary')
+        messages.success(request, 'Item updated')
+        return response
